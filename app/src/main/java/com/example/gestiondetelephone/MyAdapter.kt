cbd5b23fa292b480688d70f823c1eb8320fecbad
iheb <ihebmeftah@outlook.fr>
2021-09-28 17:30:26 +0100

    new

---
+++ app/src/main/java/com/example/gestiondetelephone/MyAdapter.kt
@@ -0,0 +1,4 @@
+package com.example.gestiondetelephone
+
+class MyAdapter {
+}
---
+++ app/src/main/java/com/example/gestiondetelephone/ajouterTelephone.kt
@@ -0,0 +1,11 @@
+package com.example.gestiondetelephone
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ajouterTelephone : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ajouter_telephone)
+    }
+}
